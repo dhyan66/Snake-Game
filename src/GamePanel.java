792264eeec60e6e8a4d481f7a597f0b26467da98
dhyan66 <94802145+dhyan66@users.noreply.github.com>
2024-05-28 19:51:15 -0300

    Day 1 Snake Game commit 2

---
+++ src/GamePanel.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.random.*;
+public class GamePanel extends JPanel implements ActionListener{
+    static final int screen_width= 400;
+    static final int screen_height = 800;
+    static final int unit_size = 25;
+    static final int game_units = (screen_width*screen_height)/unit_size;
+    static final int delay =75;
+    GamePanel() {
+    }
+
+    public void startGame(){
+
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw (Graphics g){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void checkApple(){
+
+    }
+    public  void checkCollisions(){
+
+    }
+
+    public void gameOver(Graphics g){
+
+    }
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public class MyKeyAdapter extends keyAdapter{
+        @Override
+        public void keyPressed(keyEvent e){
+
+        }
+    }
+
+
+
+}
