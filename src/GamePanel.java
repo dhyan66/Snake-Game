a1ba4575a343da7f523e8a59d92d008b79f6ed8e
dhyan66 <94802145+dhyan66@users.noreply.github.com>
2024-05-29 19:20:14 -0300

    Day 2 - game panel created

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,17 +1,38 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.util.random.*;
+import java.util.Random;
+import javax.swing.JPanel;
 public class GamePanel extends JPanel implements ActionListener{
-    static final int screen_width= 400;
-    static final int screen_height = 800;
-    static final int unit_size = 25;
+    static final int screen_width= 600;
+    static final int screen_height = 600;
+    static final int unit_size = 50;
     static final int game_units = (screen_width*screen_height)/unit_size;
     static final int delay =75;
+    final int x[] = new int[game_units];
+    final int y[] = new int[game_units];
+    int bodyparts =6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
     GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(screen_width,screen_height));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
 
     public void startGame(){
+        newApple();
+        running = true;
+        timer = new Timer(delay,this);
+        timer.start();
 
     }
 
@@ -19,6 +40,16 @@ public class GamePanel extends JPanel implements ActionListener{
 
     }
     public void draw (Graphics g){
+        for (int i=0;i<screen_height/unit_size;i++){
+            g.drawLine(i*unit_size,0,i*unit_size,screen_height);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX,appleY,unit_size,unit_size);
+
+    }
+    public void newApple(){
+        appleX = random.nextInt((int)(screen_width/unit_size))*unit_size;
+        appleY = random.nextInt((int)(screen_height/unit_size))*unit_size;
 
     }
 
@@ -40,13 +71,13 @@ public class GamePanel extends JPanel implements ActionListener{
 
     }
 
-    public class MyKeyAdapter extends keyAdapter{
+    public class MyKeyAdapter extends KeyAdapter{
         @Override
-        public void keyPressed(keyEvent e){
+        public void keyPressed(KeyEvent e){
 
         }
     }
 
 
-
 }
+
